3704a857d85b8526b6da25ffb2e6c786f2c00693
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-03-12 18:29:01 -0500

    Add files via upload

---
+++ language_translastor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 18:22:00 2023
+
+@author: 13m0nD3m0n
+"""
+
+root.config(bg = '#F2CCC3')
+
+input_label.place(relx = 0.02, rely = 0.2, anchor=W)
+Input_text.placec(relx=0.02,rely=0.05,anchor=W)
